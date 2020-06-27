011861dc35d208f7623b26a0d405ee1c69435f75
Marineford2410 <marconava2410@gmail.com>
2020-06-27 16:43:09 -0500

    Acompletando pantalla principal

--- lib/main.dart
+++ lib/main.dart
@@ -29,169 +29,205 @@ class HomeScreen extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
 		return Scaffold(
-			body: Column(
-				children: < Widget > [
-					ClipPath(
-						clipper: MyClipper(),
-						child: Container(
-							padding: EdgeInsets.only(left: 40, top: 50, right: 20),
-							height: 350,
-							width: double.infinity,
-							decoration: BoxDecoration(
-								gradient: LinearGradient(
-									begin: Alignment.centerRight,
-									end: Alignment.bottomLeft,
-									colors: [
-										Color(0xFF3383CD),
-										Color(0xFF11249F),
-									]
-								),
-								image: DecorationImage(
-									image: AssetImage("assets/images/virus.png"),
-								)
-							),
-							child: Column(
-								crossAxisAlignment: CrossAxisAlignment.start,
-								children: < Widget > [
-									Align(
-										alignment: Alignment.topRight,
-										child: SvgPicture.asset("assets/icons/menu.svg"),
-									),
-									SizedBox(height: 20),
-									Expanded(
-										child: Stack(
-											children: < Widget > [
-												SvgPicture.asset(
-													"assets/icons/Drcorona.svg",
-													width: 230,
-													fit: BoxFit.fitWidth,
-													alignment: Alignment.topCenter,
-												),
-												Positioned(
-													top: 20,
-													left: 150,
-													child: Text(
-														"All you need \nis stay at home.",
-														style: kHeadingTextStyle.copyWith(
-															color: Colors.white
-														),
-													),
-												),
-												Container(),
-											],
-										),
-									),
-								],
-							),
-						),
-					),
-					Container(
-						margin: EdgeInsets.symmetric(horizontal: 20),
-						padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-						height: 60,
-						width: double.infinity,
-						decoration: BoxDecoration(
-							color: Colors.white,
-							borderRadius: BorderRadius.circular(25),
-							border: Border.all(
-								color: Color(0xFFE5E5E5)
-							),
-						),
-						child: Row(
-							children: < Widget > [
-								SvgPicture.asset("assets/icons/maps-and-flags.svg"),
-								SizedBox(width: 20),
-								Expanded(
-									child: DropdownButton(
-										isExpanded: true,
-										underline: SizedBox(),
-										icon: SvgPicture.asset("assets/icons/dropdown.svg"),
-										value: 'Indonecia',
-										items: ['Indonecia', 'Bangladesh', 'United States', 'México', 'Japan']
-										.map < DropdownMenuItem < String >> ((String value) {
-											return DropdownMenuItem < String > (
-												value: value,
-												child: Text(value),
-											);
-										}).toList(),
-										onChanged: (value) {},
-									),
-								),
-							],
-						),
-					),
-					SizedBox(height: 20),
-					Padding(
-						padding: EdgeInsets.symmetric(horizontal: 20),
-						child: Column(
-							children: < Widget > [
-								Row(
-									children: < Widget > [
-										RichText(
-											text: TextSpan(
-												children: [
-													TextSpan(
-														text: "Case Update\n",
-														style: kTitleTextStyle,
-													),
-													TextSpan(
-														text: "Newest update March 28",
-														style: TextStyle(
-															color: kTextLightColor
-														),
-													),
-												],
-											),
-										),
-										Spacer(),
-										Text(
-											"See details",
-											style: TextStyle(
-												color: kPrimaryColor,
-												fontWeight: FontWeight.w600
-											),
-										),
-									],
-								),
-								SizedBox(height: 20),
-								Container(
-									padding: EdgeInsets.all(20),
-									decoration: BoxDecoration(
-										borderRadius: BorderRadius.circular(20),
-										color: Colors.white,
-										boxShadow: [
-											BoxShadow(
-												offset: Offset(0, 4),
-												blurRadius: 30,
-												color: kShadowColor
-											),
-										],
-									),
-									child: Row(
-										mainAxisAlignment: MainAxisAlignment.spaceBetween,
-										children: < Widget > [
-											Counter(
-												color: kInfectedColor,
-												number: 1046,
-												title: "Infected",
-											),
-											Counter(
-												color: kDeathColor,
-												number: 87,
-												title: "Deaths",
-											),
-											Counter(
-												color: kRecovercolor,
-												number: 46,
-												title: "Recovered",
-											),
-										],
-									),
-								),
-							],
-						),
-					),
-				],
+			body: ListView(
+        children: <Widget> [
+          Column(
+            children: < Widget > [
+              ClipPath(
+                clipper: MyClipper(),
+                child: Container(
+                  padding: EdgeInsets.only(left: 40, top: 50, right: 20),
+                  height: 350,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.centerRight,
+                      end: Alignment.bottomLeft,
+                      colors: [
+                        Color(0xFF3383CD),
+                        Color(0xFF11249F),
+                      ]
+                    ),
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/virus.png"),
+                    )
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: < Widget > [
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: SvgPicture.asset("assets/icons/menu.svg"),
+                      ),
+                      SizedBox(height: 20),
+                      Expanded(
+                        child: Stack(
+                          children: < Widget > [
+                            SvgPicture.asset(
+                              "assets/icons/Drcorona.svg",
+                              width: 230,
+                              fit: BoxFit.fitWidth,
+                              alignment: Alignment.topCenter,
+                            ),
+                            Positioned(
+                              top: 20,
+                              left: 150,
+                              child: Text(
+                                "All you need \nis stay at home.",
+                                style: kHeadingTextStyle.copyWith(
+                                  color: Colors.white
+                                ),
+                              ),
+                            ),
+                            Container(),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                height: 60,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25),
+                  border: Border.all(
+                    color: Color(0xFFE5E5E5)
+                  ),
+                ),
+                child: Row(
+                  children: < Widget > [
+                    SvgPicture.asset("assets/icons/maps-and-flags.svg"),
+                    SizedBox(width: 20),
+                    Expanded(
+                      child: DropdownButton(
+                        isExpanded: true,
+                        underline: SizedBox(),
+                        icon: SvgPicture.asset("assets/icons/dropdown.svg"),
+                        value: 'Indonecia',
+                        items: ['Indonecia', 'Bangladesh', 'United States', 'México', 'Japan']
+                        .map < DropdownMenuItem < String >> ((String value) {
+                          return DropdownMenuItem < String > (
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (value) {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: < Widget > [
+                    Row(
+                      children: < Widget > [
+                        RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: "Case Update\n",
+                                style: kTitleTextStyle,
+                              ),
+                              TextSpan(
+                                text: "Newest update March 28",
+                                style: TextStyle(
+                                  color: kTextLightColor
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        Text(
+                          "See details",
+                          style: TextStyle(
+                            color: kPrimaryColor,
+                            fontWeight: FontWeight.w600
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 4),
+                            blurRadius: 30,
+                            color: kShadowColor
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: < Widget > [
+                          Counter(
+                            color: kInfectedColor,
+                            number: 1046,
+                            title: "Infected",
+                          ),
+                          Counter(
+                            color: kDeathColor,
+                            number: 87,
+                            title: "Deaths",
+                          ),
+                          Counter(
+                            color: kRecovercolor,
+                            number: 46,
+                            title: "Recovered",
+                          ),
+                        ],
+                      ),
+                    ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Spread of Virus', style: kTitleTextStyle),
+                      Text(
+                        'See details',
+                        style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.w600
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    padding: EdgeInsets.all(20),
+                    height: 170,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 30,
+                          color: kShadowColor
+                        )
+                      ],
+                    ),
+                    child: Image.asset("assets/images/map.png", fit: BoxFit.contain),
+                  ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ]
 			),
 		);
 	}
